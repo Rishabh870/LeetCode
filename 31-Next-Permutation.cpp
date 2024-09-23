d370e6073aa3a742dffb059b6da7801b9fc8727a
Rishabh870 <67893503+Rishabh870@users.noreply.github.com>
2024-09-23 01:25:54 -0700

    [31] [Time Beats: 14.88%] [Memory Beats: 7.18%] - LeetPush

---
+++ 31-Next-Permutation.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+
+        int a=0,b=-1,n=nums.size();
+
+        for(int i = n-2; i>=0;i--){
+         
+            if(nums[i]<nums[i+1]){
+                b=i; 
+                break;
+            }
+
+        }
+
+        if(b==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+
+         for(int i = n-1; i>b;i--){
+        
+            if(nums[i]>nums[b]){
+                swap(nums[i],nums[b]);
+                break;
+
+            }
+
+        }
+
+        reverse(nums.begin()+b+1,nums.end());
+
+
+       
+
+
+        
+    }
+};
+
